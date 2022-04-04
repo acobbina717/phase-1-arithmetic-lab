d478f9ffb45c155d46809509c81cb6368d77edde
Abeku <acobbina717@gmail.com>
2022-04-04 14:56:39 -0400

    commit tree 5f5bf40be6c17cf94765e641c81e552d371a8da3

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+const multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 11)
+
+let num3 = 13
+let num4 = 9
+const mod = num3 % num4
+
+const max = Math.max(0, 5, 10, 15, 20)
